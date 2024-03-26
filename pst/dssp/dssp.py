c5c112576aab582a991e05e06704bed45f69fa22
Kyle Hippe <kyle.hippe@gmail.com>
2024-03-25 19:06:25 -0500

    Huge update, can render descriptive text from a single PDB, need to establish CLI and workflow

--- pst/dssp/dssp.py
+++ pst/dssp/dssp.py
@@ -1,19 +1,29 @@
-from __future__ import annotations
-from pathlib import Path
-from typing import Any, List, TypeVar, Union
-from concurrent.futures import ProcessPoolExecutor
-import json
+"""DSSP module, hooks into the DSSP executable and parses its output."""
 
-from pydantic import BaseModel, model_validator, field_validator
+from __future__ import annotations
+
+import json
+import subprocess
+from concurrent.futures import ProcessPoolExecutor
+from pathlib import Path
+from typing import Any
+from typing import Optional
+from typing import TypeVar
+from typing import Union
+
+from pydantic import BaseModel
+from pydantic import field_validator
+from pydantic import model_validator
 
 from pst.output import OutputType
 
 T = TypeVar("T", bound="BaseModel")
 PathLike = Union[str, Path]
 
-__all__ = ["Protein", "Residue", "process_dssp"]
+__all__ = ["Protein", "Residue", "process_dssp", "run_and_parse_dssp"]
 
 
+# TODO: believe it or not the dataclass is cumbersome... figure out how to cleanup with respect to the necessary output
 class Residue(BaseModel):
     """Container for a residue as it is output from DSSP"""
 
@@ -28,8 +38,8 @@ class Residue(BaseModel):
     ss: str
     """Secondary structure single letter code (7 designations)"""
     acc: int
-    """Solvent accessability (unsure of scaling here)"""
-    h_bonds: Union[List[float], str]
+    """Solvent accessibility (unsure of scaling here)"""
+    h_bonds: Union[list[float], str]
     """List of hbonds, currently unparsed because they are nasty"""
     tco: float
     """cosine of angle between C=O of residue i and C=O of residue i-1"""
@@ -69,10 +79,19 @@ class Residue(BaseModel):
 
 
 class Protein(BaseModel):
-    residues: List[Residue]
+    residues: list[Residue]
+
+    @classmethod
+    def from_str(cls: type[T], data: str) -> T:
+        return cls.parse_data(data)
 
     @classmethod
     def from_dssp(cls: type[T], path: PathLike) -> T:
+        data = path.read_text()
+        return cls.parse_data(data)
+
+    @classmethod
+    def parse_data(cls: type[T], data: str) -> T:
         # How/where to grab fields from the dssp files, can view at
         # https://web.archive.org/web/20230305032352/https://swift.cmbi.umcn.nl/gv/dssp/DSSP_3.html
         fields = {
@@ -81,16 +100,19 @@ class Protein(BaseModel):
             "chain": slice(10, 12),  # Chain index (letter based)
             "residue": slice(12, 16),  # Single residue letter # code
             "ss": slice(16, 17),  # Secondary structure single letter code
-            "acc": slice(35, 38),  # Solvent accessability
+            "acc": slice(35, 38),  # Solvent accessibility
             "h_bonds": slice(38, 85),  # string list of hbonds
             "tco": slice(
-                85, 91
+                85,
+                91,
             ),  # cosine of angle between C=O of residue i and C=O of residue i-1
             "kappa": slice(
-                91, 97
+                91,
+                97,
             ),  # virtual bond angle (bend angle) defined by the three C-alpha atoms of residues i-2,i,i+2.
             "alpha": slice(
-                97, 103
+                97,
+                103,
             ),  # virtual torsion angle (dihedral angle) defined by the four C-alpha atoms of residues i-1,i,i+1,i+2
             "phi": slice(103, 109),  # IUPAC peptide backbone torsion angles
             "psi": slice(109, 115),  # IUPAC peptide backbone torsion angles
@@ -101,27 +123,28 @@ class Protein(BaseModel):
 
         residues = []
         past_header = False
-        with open(path, "r") as file:
-            for line in file.readlines():
-                # Need to iterate past a large/variable length header
-                if not past_header:
-                    if "#" in line.split()[0]:  # need to check if its first elem
-                        past_header = True
-                    continue
+        for line in data.strip().split("\n"):
+            # Need to iterate past a large/variable length header
+            if not past_header:
+                if "#" in line.split()[0]:  # need to check if its first elem
+                    past_header = True
+                continue
 
-                residue = Residue(**{f: line[e] for f, e in fields.items()})
+            residue = Residue(**{f: line[e] for f, e in fields.items()})
 
-                # A '!' specifies a break in the chain or invalid residue
-                if "!" in residue.residue:
-                    continue
+            # A '!' specifies a break in the chain or invalid residue
+            if "!" in residue.residue:
+                continue
 
-                residues.append(residue)
+            residues.append(residue)
 
         return cls(residues=residues)
 
 
 def output_json(
-    parsed_data: dict[str, Protein], output_file: Path, fields: list[str]
+    parsed_data: dict[str, Protein],
+    output_file: Path,
+    fields: list[str],
 ) -> None:
     """Output single jsonl file with key being the file it came from (without suffix) and the values being a dictionary of lists of included fields
 
@@ -158,7 +181,6 @@ def process_dssp(
     fields: list[str] | None = None,
     num_cpus: int = 1,
 ) -> None:
-
     files = list(dssp_dir.glob(glob_pattern))
 
     data = {}
@@ -182,9 +204,81 @@ def process_dssp(
                 localout = {}
                 for k in fields:
                     localout[k] = " ".join(
-                        [str(getattr(elem, k)) for elem in prot.residues]
+                        [str(getattr(elem, k)) for elem in prot.residues],
                     )
                 print(f"{filename}:")
                 for k, v in localout.items():
                     print(f"\t{k}: {v}")
                 print("\n")
+
+
+def run_and_parse_dssp(
+    pdb_file: Path,
+    dssp_path: Path,
+    fields: Optional[list[str]] = None,
+) -> dict[str, Any]:
+    """Run DSSP and parse the output into a dataclass
+
+    Parameters
+    ----------
+    pdb_file : Path
+        Path to the pdb/mmcif file
+    dssp_path : Path
+        Path to the DSSP executable
+
+    Returns
+    -------
+    str
+        The contents of the output
+    """
+    cmd = f"{dssp_path} -i {pdb_file}"
+    res = subprocess.run(cmd.split(), capture_output=True, check=False)
+
+    assert res.returncode == 0, f"Error running DSSP: {res.stderr}"
+
+    dssp_data = res.stdout.decode("utf-8")
+    protein = Protein.from_str(dssp_data)
+
+    if fields is None:
+        fields = list(Residue.model_fields.keys())
+
+    structure_data = {}
+    for k in fields:
+        structure_data[k] = [getattr(elem, k) for elem in protein.residues]
+
+    # Hard coded calculations for now but we need this for the `context`
+    aa_len = len(protein.residues)
+    helix_code = ["H", "G", "I"]
+    beta_code = ["E", "B"]
+    coil_code = ["T", "S", " "]
+    alpha_percent = (
+        sum([1 for ss in structure_data["ss"] if ss in helix_code]) / aa_len
+    ) * 100
+    beta_percent = (
+        sum([1 for ss in structure_data["ss"] if ss in beta_code]) / aa_len
+    ) * 100
+    coil_percent = (
+        sum([1 for ss in structure_data["ss"] if ss in coil_code]) / aa_len
+    ) * 100
+
+    structure_data["alpha_percent"] = alpha_percent
+    structure_data["beta_percent"] = beta_percent
+    structure_data["coil_percent"] = coil_percent
+
+    most_common = [
+        ("alpha helix", alpha_percent),
+        ("beta sheet", beta_percent),
+        ("coil", coil_percent),
+    ]
+    most_common = sorted(most_common, key=lambda x: x[1], reverse=True)
+    structure_data["ordered_ss"] = most_common
+
+    return structure_data
+
+
+if __name__ == "__main__":
+    pdb_path = Path("/home/khippe/small-structs/1000001/AF-F8U1P7-F1-model_v4.cif")
+
+    dssp_path = Path("/home/khippe/miniforge3/envs/dssp/bin/mkdssp")
+
+    print(run_and_parse_dssp(pdb_path, dssp_path))
--- pst/stats/stats.py
+++ pst/stats/stats.py
@@ -1,20 +1,45 @@
 from __future__ import annotations
-from pathlib import Path
-import gzip
-from concurrent.futures import ProcessPoolExecutor
-from collections import Counter
-from typing import Any
-import json
 
-from Bio.PDB import MMCIFParser, PDBParser
-from Bio.PDB.Structure import Structure
-from Bio.SeqUtils.IsoelectricPoint import IsoelectricPoint
-from Bio.SeqUtils import seq1
+import gzip
+import json
+from collections import Counter
+from concurrent.futures import ProcessPoolExecutor
+from pathlib import Path
+from typing import Any
+
+from Bio.PDB import MMCIFParser
+from Bio.PDB import PDBParser
 from Bio.PDB.SASA import ShrakeRupley
+from Bio.PDB.Structure import Structure
+from Bio.SeqUtils import seq1
+from Bio.SeqUtils.IsoelectricPoint import IsoelectricPoint
 
 from pst.output import OutputType
 
-__all__ = ["structure_statistics"]
+__all__ = ['structure_statistics', 'get_structure_statistics']
+
+AA_NAME_CONVERSION = {
+    'A': 'Alanine',
+    'R': 'Arginine',
+    'N': 'Asparagine',
+    'D': 'Aspartic Acid',
+    'C': 'Cysteine',
+    'E': 'Glutamic Acid',
+    'Q': 'Glutamine',
+    'G': 'Glycine',
+    'H': 'Histidine',
+    'I': 'Isoleucine',
+    'L': 'Leucine',
+    'K': 'Lysine',
+    'M': 'Methionine',
+    'F': 'Phenylalanine',
+    'P': 'Proline',
+    'S': 'Serine',
+    'T': 'Threonine',
+    'W': 'Tryptophan',
+    'Y': 'Tyrosine',
+    'V': 'Valine',
+}
 
 
 def get_structure(fp: Path) -> Structure:
@@ -30,19 +55,18 @@ def get_structure(fp: Path) -> Structure:
     Structure
         Biopython Structure object representing the structure
     """
-
-    struct_id = str(fp).rstrip("".join(fp.suffixes))
-    if ".pdb" in fp.suffixes:
+    struct_id = str(fp).rstrip(''.join(fp.suffixes))
+    if '.pdb' in fp.suffixes:
         parser = PDBParser(QUIET=True)
-    elif ".cif" in fp.suffixes:
+    elif '.cif' in fp.suffixes:
         parser = MMCIFParser(QUIET=True)
     else:
-        raise ValueError(f"File {fp} type not supported")
+        raise ValueError(f'File {fp} type not supported')
 
-    if ".gz" in fp.suffixes:
-        file = gzip.open(fp, "rt")
+    if '.gz' in fp.suffixes:
+        file = gzip.open(fp, 'rt')
     else:
-        file = open(fp, "r")
+        file = open(fp)
 
     structure = parser.get_structure(struct_id, file)
 
@@ -51,6 +75,7 @@ def get_structure(fp: Path) -> Structure:
     return structure
 
 
+# TODO: might be good to parameterize some of the fields (e.g top N)
 def get_structure_statistics(fp: Path) -> dict[str, str]:
     """Get statistics from a structure file
 
@@ -64,33 +89,55 @@ def get_structure_statistics(fp: Path) -> dict[str, str]:
     dict[str, str]
         Dictionary of statistics from the structure
     """
-
     structure = get_structure(fp)
-    amino_acids = seq1("".join([res.resname for res in structure.get_residues()]))
+    amino_acids = seq1(
+        ''.join([res.resname for res in structure.get_residues()]),
+    )
 
     structure_output = {}
     # Simple statistics
-    structure_output["num_models"] = len(structure)
-    structure_output["num_chains"] = len([chain for chain in structure.get_chains()])
-    structure_output["num_residues"] = len([res for res in structure.get_residues()])
-    structure_output["num_atoms"] = len([atom for atom in structure.get_atoms()])
-    structure_output["amino_acid_count"] = dict(Counter(amino_acids))
+    structure_output['num_models'] = len(structure)
+    structure_output['num_chains'] = len(
+        [chain for chain in structure.get_chains()],
+    )
+    structure_output['num_residues'] = len(
+        [res for res in structure.get_residues()],
+    )
+    structure_output['num_atoms'] = len(
+        [atom for atom in structure.get_atoms()],
+    )
+    structure_output['amino_acid_count'] = dict(Counter(amino_acids))
+    # top 3 most common amino acids and their total percentage
+    total_aa = structure_output['num_residues']
+    common_residues = sorted(
+        structure_output['amino_acid_count'].items(),
+        key=lambda x: x[1],
+        reverse=True,
+    )
+    structure_output['top3_residues'] = [
+        AA_NAME_CONVERSION[res[0]] for res in common_residues[:3]
+    ]
+    structure_output['top3_percent'] = (
+        sum(res[1] for res in common_residues[:3]) / total_aa
+    ) * 100
 
     # Isoelectic point + various related info
     ip = IsoelectricPoint(amino_acids)
-    structure_output["isoelectric_point"] = ip.pi()
-    # structure_output['charged_aa_content'] = ip.charged_aas_content
+    structure_output['isoelectric_point'] = ip.pi()
 
     # Solvent accessible surface area
     sr = ShrakeRupley()
-    sr.compute(structure, level="S")
-    structure_output["sasa"] = structure.sasa
+    sr.compute(structure, level='S')
+    sr.compute(structure, level='R')
+    structure_output['sasa'] = structure.sasa
 
     return structure_output
 
 
 def output_json(
-    parsed_data: dict[str, Any], output_file: Path, fields: list[str]
+    parsed_data: dict[str, Any],
+    output_file: Path,
+    fields: list[str],
 ) -> None:
     """Output JSON representation of PDB structure general statistics
 
@@ -102,7 +149,7 @@ def output_json(
         Path to save overall output file
     """
     output_data = {k: v for k, v in parsed_data.items() if k in fields}
-    with open(output_file, "w") as f:
+    with open(output_file, 'w') as f:
         json.dump(output_data, f)
 
 
@@ -118,7 +165,6 @@ def structure_statistics(
     fields: list[str] | None = None,
     num_cpus: int = 1,
 ) -> None:
-
     if input_dir.is_file():
         files = [input_dir]
     elif input_dir.is_dir():
@@ -127,15 +173,20 @@ def structure_statistics(
     data = {}
     # TODO: Same as dssp, think about streaming to an output format here
     with ProcessPoolExecutor(max_workers=num_cpus) as executor:
-        for fp, res in zip(files, executor.map(get_structure_statistics, files)):
-            struct_name = str(fp).rstrip("".join(fp.suffixes))
+        for fp, res in zip(
+            files,
+            executor.map(get_structure_statistics, files),
+        ):
+            struct_name = str(fp).rstrip(''.join(fp.suffixes))
             data[struct_name] = res
 
     match output_format:
         case OutputType.json:
-            assert output is not None, "Output file must be specified for json output"
+            assert (
+                output is not None
+            ), 'Output file must be specified for json output'
             output_json(data, output, fields)
 
         case OutputType.stdout:
             for fname, stats in data.items():
-                print(f"{Path(fname).stem}: {stats}")
+                print(f'{Path(fname).stem}: {stats}')
